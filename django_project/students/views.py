82bfe7b172ee2e5d3efc34be5933d1bcf9639cec
nisalmissaka <wishwakanisal@gmail.com>
2026-06-02 18:18:01 +0530

    Create Navbar is project

--- django_project/students/views.py
+++ django_project/students/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from .models import Student
 
 # Create your views here.
 
 def index(request):
-    return render(request,'students/index.html')
+    return render(request,'students/index.html',{
+        'students' : Student.objects.all()
+    })
     
