5f861d3ee7fa33b5c0976fbd92d4e8b826fdafdd
nisalmissaka <wishwakanisal@gmail.com>
2026-06-03 16:03:10 +0530

    create to icon button

--- django_project/students/views.py
+++ django_project/students/views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
 from .models import Student
 
 # Create your views here.
@@ -7,4 +9,8 @@ def index(request):
     return render(request,'students/index.html',{
         'students' : Student.objects.all()
     })
+
+def view_student(request, id) :
+    student = Student.object.get(pk=id)  
+    return HttpResponseRedirect(reverse('index'))
     
